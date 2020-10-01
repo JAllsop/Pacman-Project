7c98453829a97fc8c3599a4cb3d6b909b89b726c
James Allsop <james.allsop8@gmail.com>
2020-10-01 22:09:44 +0200

    Basic Player Movement Test

--- test-source-code/pacman-tests.cpp
+++ test-source-code/pacman-tests.cpp
@@ -3,6 +3,8 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest.h"
 
+#define l 40.0f
+
 TEST_CASE("maze test")
 {
     std::shared_ptr<sf::RenderWindow> window;
@@ -12,12 +14,26 @@ TEST_CASE("maze test")
     window->setVerticalSyncEnabled(true);
 }
 
-TEST_CASE("test down movement")
+TEST_CASE("Player Basic Movement")
 {
-    auto testPlayer = Player(40.0, 40.0, sf::Color::Green);
+    auto testPlayer = Player(l, l, sf::Color::Blue);
+    testPlayer.setPosition(500, 500);
+
     auto y = testPlayer.getY();
     testPlayer.moveDown();
-    CHECK(y +1 == testPlayer.getY());
+    CHECK(y + l == testPlayer.getY());
+
+    y = testPlayer.getY();
+    testPlayer.moveUp();
+    CHECK(y - l == testPlayer.getY());
+
+    auto x = testPlayer.getX();
+    testPlayer.moveRight();
+    CHECK(x + l == testPlayer.getX());
+
+    x = testPlayer.getX();
+    testPlayer.moveLeft();
+    CHECK(x - l == testPlayer.getX());
 }
 
 /*
